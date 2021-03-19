4f2a06bf842bcb7c8fe80c2878d99b157ad4a5a9
AshleyCahalan <a_cahalan@fanshaweonline.ca>
2021-03-19 17:18:37 -0400

    stuff

---
+++ js/main.js
@@ -0,0 +1,36 @@
+// IIFE => research on MDN (also called the module pattern)
+(() => {
+  // select all of the keyboard key graphics
+  const keyGraphics = document.querySelectorAll('.key');
+
+  // functions go in the middle
+  // get the keycode so that we can match it to an audio element
+  function catchKeyCode(event) {
+    //let key = event.keyCode;
+    // JavaScript template string => MDN
+    let audio = document.querySelector(`audio[data-key="${event.keyCode}"]`),
+        currentKey = document.querySelector(`div[data-key="${event.keyCode}"]`);
+
+    // if there's no matching audio element, then quit
+    if (!audio) { return; }
+
+    // animate the div that corresponds to the key we pressed
+    currentKey.classList.add("playing");
+
+    //debugger;
+    audio.currentTime = 0;
+    audio.play();
+  }
+
+  // remove the playing class after the transition fires the first time
+  function resetKey(event) {
+    // debugger;
+    if (event.propertyName === "transform") {
+      this.classList.remove("playing");
+    }
+  }
+  // event handling goes at the bottom
+  keyGraphics.forEach(key => key.addEventListener("transitionend", resetKey));
+  //
+  window.addEventListener("keyup", catchKeyCode);
+})();
